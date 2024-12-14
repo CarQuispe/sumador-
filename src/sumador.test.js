bfc9fa1b93c5f783e94f3f803c7faca6572f4cb9
RosaR20 <rosareynaga2025@gmail.com>
2024-12-14 02:38:42 -0400

    suma ceros

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -6,7 +6,12 @@ describe("Sumar", () => {
   });
   it("debería sumar un número positivo y un negativo", () => {
     expect(sumar(3, -2)).toEqual(1);
-  })
+  });
+
+  it("debería manejar ceros correctamente", () => {
+    expect(sumar(0, 0)).toEqual(0);
+    expect(sumar(5, 0)).toEqual(5);
+  });
 });
 
 
