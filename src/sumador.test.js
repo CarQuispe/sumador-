62cc942d0b5d70359a3fb41c4ea951078aa53a49
RosaR20 <rosareynaga2025@gmail.com>
2024-12-14 02:37:19 -0400

    suma mixto

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,6 +4,9 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+  it("debería sumar un número positivo y un negativo", () => {
+    expect(sumar(3, -2)).toEqual(1);
+  })
 });
 
 
